5a91f3ed32165275b8603f4cdfc3e70e11415394
Brian <wamwangimathenge@gmail.com>
2023-01-16 15:12:53 +0300

    Handle Add Task

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+
 
 function App() {
+
+
+  const [tasks, setTasks] = useState("Tasks")
+  const [category, setCategory] = useState("All")
+
+
+  function handleAddTask(addTask) {
+    setTasks([...tasks, addTask])
+  }
+
+
+
+
   return (
     <div className="App">
       <h2>My tasks</h2>
