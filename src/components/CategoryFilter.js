24a7e3efcdf27effd7c6e38d5539b8f83a18aa5e
Brian <wamwangimathenge@gmail.com>
2023-01-16 15:20:32 +0300

    Category Filter

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,6 +1,21 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ categories, selectedCategory, onSelectCategory }) {
+  const categoryButton = categories.map((category) => {
+    const className = category === selectedCategory ? "selected" : null
+
+    return (
+      <button
+        key={category}
+        className={className}
+        onAnimationEndCaptureClick={() => onSelectCategory(category)}  
+      >
+        {category}
+      </button>
+    )
+
+
+  })
   return (
     <div className="categories">
       <h5>Category filters</h5>
